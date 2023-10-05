331a7980c5ff48aacb748a0c0228f8184dad5c54
Andresaki <106437891+andresaki@users.noreply.github.com>
2023-10-04 19:48:48 -0500

    Delete NetBeansProjects directory

--- NetBeansProjects/abuelo/src/main/java/com/ciclo2/abuelo/Abuelo.java
+++
@@ -1,8 +0,0 @@
-package com.ciclo2.abuelo;
-
-public class Abuelo {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- NetBeansProjects/estudiantes/src/main/java/com/ciclo2/estudiantes/main/Main.java
+++
@@ -1,8 +0,0 @@
-package com.ciclo2.estudiantes.main;
-
-public class Main {
-    
-    public static void main(String[] args) {
-        
-    }
-}
